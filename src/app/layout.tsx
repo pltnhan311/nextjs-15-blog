b8e8e4151db9e589c992cc5f7d5ee9bbf6f65d0a
pltnhan311 <92429264+pltnhan311@users.noreply.github.com>
2025-01-05 11:06:51 +0700

    create sanity schema

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,27 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Work_Sans } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono, Work_Sans } from 'next/font/google';
+import './globals.css';
+import 'easymde/dist/easymde.min.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 const workSans = Work_Sans({
-  variable: "--font-work-sans",
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: '--font-work-sans',
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 });
 
 export const metadata: Metadata = {
-  title: "YC Directory",
-  description: "Pitch, Vote and Grow",
+  title: 'YC Directory',
+  description: 'Pitch, Vote and Grow',
 };
 
 export default function RootLayout({
--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from 'sanity';
+import { author } from 'src/sanity/schemaTypes/author';
+import { startup } from 'src/sanity/schemaTypes/startup';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [author, startup],
+};
