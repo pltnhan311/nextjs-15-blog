dd60d5d3b9f2c9aa575ef82b4654214643e97de1
pltnhan311 <92429264+pltnhan311@users.noreply.github.com>
2025-01-07 19:37:14 +0700

    setup sanity write client

--- src/app/components/View.tsx
+++ src/app/components/View.tsx
@@ -2,11 +2,20 @@ import Ping from '@/components/Ping';
 import React from 'react';
 import { client } from 'src/sanity/lib/client';
 import { STARTUP_VIEWS_QUERY } from 'src/sanity/lib/queries';
+import { writeClient } from 'src/sanity/lib/write-client';
+import { after } from 'next/server';
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
+
+  after(async () => {
+    await writeClient
+      .patch(id)
+      .set({ views: totalViews + 1 })
+      .commit();
+  });
 
   return (
     <div className="view-container">
--- src/sanity/lib/write-client.ts
+++ src/sanity/lib/write-client.ts
@@ -1,15 +1,21 @@
-import { createClient } from 'next-sanity'
+import 'server-only';
 
-import { apiVersion, dataset, projectId } from '../env'
+import { createClient } from 'next-sanity';
+import { apiVersion, dataset, projectId, token } from '../env';
 
-export const client = createClient({
+export const writeClient = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true,
+  token,
+  useCdn: false,
   // Set `useCdn` to `false` when you need the latest data, such as when:
   // - Statically generating pages (SSG) at build time.
   // - Using Incremental Static Regeneration (ISR) to update pages after build.
   // - Implementing tag-based revalidation to refresh content dynamically.
   // Otherwise, keep it `true` for better performance with cached data.
 });
+
+if (!writeClient.config().token) {
+  throw new Error('Missing write token');
+}
